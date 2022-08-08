249de4574e76c8b0970d01dd7d24b7cb90706486
Cheng-Ying Wu <wuchengy@oregonstate.edu>
2022-08-09 02:08:20 +0800

    update run.sh

--- Run_Scripts/run.sh
+++ Run_Scripts/run.sh
@@ -6,7 +6,7 @@ for l in 0.001 0.0001 0.00001
 do
     for b in 32 16 8 4
     do
-        python ../train.py --id=$i --lr=$l --batch_size=$b
+        python main.py --id=$i --lr=$l --batch_size=$b --feature_type=mel_spectrogram
         let "i+=1"
     done
 done
